f2a1eaf61031857786e97f51cf2b623eaf45ab7b
hana-nazer <hanapp9966@gmail.com>
2023-05-11 09:29:20 +0530

    review resort by admin

--- server/controllers/adminController/adminAuthController.js
+++ server/controllers/adminController/adminAuthController.js
@@ -1,6 +1,7 @@
 const Admin = require("../../models/adminModel");
 const { createToken } = require("../../middlewares/tokenAuth");
 const bcrypt = require("bcryptjs");
+const Resort = require('../../models/resortModel')
 
 
   // login admin
@@ -46,4 +47,38 @@ const bcrypt = require("bcryptjs");
     }
   }
 
+  // review resort
+  exports.reviewResort = async (req, res) => {
+    try {
+      const resortId = req.params.resortId;
+      const status = req.body.status;
+  
+      const resort = await Resort.findById(resortId);
+  
+      if (!resort) {
+        return res.send({
+          success: false,
+          message: "Resort not found",
+        });
+      }
+  
+      // Update the resort's status based on the provided value
+      resort.status = status;
+  
+      const updatedResort = await resort.save();
+  
+      res.send({
+        success: true,
+        message: "Resort review completed successfully",
+        data: updatedResort,
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+  
+  
   
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const adminAuthController = require('../controllers/adminController/adminAuthController')
+const adminAuthController = require('../controllers/adminController/adminAuthController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 router.post('/login',adminAuthController.adminLogin)
+router.put('/review-resort/:resortId',authMiddleware, adminAuthController.reviewResort);
+
 
 module.exports= router
