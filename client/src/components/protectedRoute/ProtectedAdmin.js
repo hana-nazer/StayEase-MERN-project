982ab73ca65d1755e9a8062ef8ada0f5f89b4353
hana-nazer <hanapp9966@gmail.com>
2023-06-17 16:04:22 +0530

    checking token expired or not

--- client/src/components/protectedRoute/ProtectedAdmin.js
+++ client/src/components/protectedRoute/ProtectedAdmin.js
@@ -1,9 +1,12 @@
-import React ,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { GetCurrentAdmin } from "../../api calls/admin";
 import { useDispatch, useSelector } from "react-redux";
 import { setAdmin } from "../../redux/getUserSlice";
+import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
-function ProtectedAdmin({children}) {
+function ProtectedAdmin({ children }) {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const admin = useSelector((state) => state.getUser.getAdmin);
   const getCurrentAdmin = async () => {
@@ -18,17 +21,20 @@ function ProtectedAdmin({children}) {
   };
 
   useEffect(() => {
-    getCurrentAdmin();
+    const token = localStorage.getItem("admin_token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (decodedToken.exp < currentTime) {
+        navigate("/admin/login");
+      } else {
+        getCurrentAdmin();
+      }
+    } else {
+      navigate("/admin/login");
+    }
   }, []);
-  return (
-    <>
-      {admin && (
-        <>
-          {children}
-        </>
-      )}
-    </>
-  );
+  return <>{admin && <>{children}</>}</>;
 }
 
 export default ProtectedAdmin;
--- client/src/components/protectedRoute/ProtectedUser.js
+++ client/src/components/protectedRoute/ProtectedUser.js
@@ -3,8 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { getCurrentUser } from "../../api calls/users";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../redux/getUserSlice";
+import jwtDecode from "jwt-decode";
 
-function ProtectedUser({children}) {
+function ProtectedUser({ children }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.getUser.getUser);
@@ -21,7 +22,18 @@ function ProtectedUser({children}) {
   };
 
   useEffect(() => {
-    getUserData();
+    const token = localStorage.getItem("user_token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (decodedToken.exp < currentTime) {
+        navigate("/login");
+      } else {
+        getUserData();
+      }
+    } else {
+      navigate("/login");
+    }
   }, []);
 
   return <>{user && <>{children}</>}</>;
