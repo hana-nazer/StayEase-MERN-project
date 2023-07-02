cedef49dcf65ae252e5c0f63ae57b6e54a4abdb0
hana-nazer <hanapp9966@gmail.com>
2023-07-02 21:08:12 +0530

    Handled error in Booking form

--- client/src/components/resort/BookingData.js
+++ client/src/components/resort/BookingData.js
@@ -1,143 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { useFormik } from "formik";
-// import moment from "moment";
-// import { useNavigate } from "react-router-dom";
-// import { useSelector, useDispatch } from "react-redux";
-// import { setBookingData } from "../../redux/BookingSlice";
-// import { disabledDateList } from "../../api calls/users";
-// import { DatePicker } from "antd";
-// import "../../fonts/fonts.css";
-
-// function BookingData() {
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
-//   const [disabledDates, setDisabledDates] = useState([]);
-//   const resortData = useSelector((state) => state.verifyResort.resortData);
-//   const formik = useFormik({
-//     initialValues: {
-//       dates: [],
-//       guests: "",
-//       name: "",
-//       phone: "",
-//     },
-//     onSubmit: async (values) => {
-//       try {
-//         const { no_of_days } = values;
-//         let bookingData = {
-//           dates: values.dates,
-//           guests: values.guests,
-//           name: values.name,
-//           phone: values.phone,
-//           no_of_days: no_of_days,
-//         };
-//         dispatch(setBookingData(bookingData));
-//         navigate("/bookingdetails");
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     },
-//   });
-//   const handleChange = (selectedDates) => {
-//     const numberOfDays = selectedDates[1].diff(selectedDates[0], "days") + 1;
-//     const startDate = moment(selectedDates[0].toDate()).format("DD-MM-YYYY");
-//     const dates = [];
-//     for (let i = 0; i < numberOfDays; i++) {
-//       const date = moment(startDate, "DD-MM-YYYY").add(i, "days");
-//       dates.push(date.format("DD-MM-YYYY"));
-//     }
-
-//     formik.setFieldValue("dates", dates);
-//     formik.setFieldValue("no_of_days", numberOfDays);
-//   };
-
-//   const fetchDisabledDates = async () => {
-//     try {
-//       const response = await disabledDateList(resortData._id);
-//       if (response.success) {
-//         console.log(response);
-//         setDisabledDates(response.disabledDates);
-//       } else {
-//         console.log(response.data.message);
-//       }
-//     } catch (error) {
-//       console.error(error.response);
-//     }
-//   };
-
-//   const disabledDate = (current) => {
-//     if (disabledDates && disabledDates.length > 0) {
-//       return disabledDates.some(
-//         (date) => current.format("DD-MM-YYYY") === date
-//       );
-//     }
-//     return false;
-//   };
-
-//   useEffect(() => {
-//     fetchDisabledDates();
-//   }, []);
-
-//   return (
-//     <form onSubmit={formik.handleSubmit}>
-//       <p className="flex items-center justify-center mb-6 text-3xl font-bold tracking-wide text-gray-300 font-oswald">
-//         Book your trip
-//       </p>
-//       <div className="mb-6" style={{ width: "100%" }}>
-//         <DatePicker.RangePicker
-//           onChange={handleChange}
-//           format="DD-MM-YYYY"
-//           name="dates"
-//           style={{ width: "100%" }}
-//           disabledDate={disabledDate}
-//         />
-//       </div>
-//       <div className="mb-6">
-//         <input
-//           className="w-full px-4 py-3 placeholder-gray-500 border rounded-lg"
-//           type="number"
-//           placeholder="No:of guests"
-//           name="guests"
-//           value={formik.values.guests}
-//           onChange={formik.handleChange}
-//           max={resortData.no_of_guest}
-//         />
-//       </div>
-//       <div className="mb-6">
-//         <input
-//           className="w-full px-4 py-3 placeholder-gray-500 border rounded-lg"
-//           type="text"
-//           placeholder="Name"
-//           name="name"
-//           value={formik.values.name}
-//           onChange={formik.handleChange}
-//         />
-//       </div>
-//       <div className="mb-6">
-//         <input
-//           className="w-full px-4 py-3 placeholder-gray-500 border rounded-lg"
-//           type="tel"
-//           placeholder="Phone"
-//           name="phone"
-//           value={formik.values.phone}
-//           onChange={formik.handleChange}
-//         />
-//       </div>
-//       <div className="text-center">
-//         <button
-//           className="w-full px-8 py-3 text-white rounded-lg bg-nav-color"
-//           type="submit"
-//         >
-//           Proceed
-//         </button>
-//       </div>
-//     </form>
-//   );
-// }
-// export default BookingData;
-
-
-
-
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import moment from "moment";
@@ -153,6 +13,33 @@ function BookingData() {
   const dispatch = useDispatch();
   const [disabledDates, setDisabledDates] = useState([]);
   const resortData = useSelector((state) => state.verifyResort.resortData);
+
+  const validateForm = (values) => {
+    const errors = {};
+
+    if (!values.dates || values.dates.length === 0) {
+      errors.dates = "Choose a date";
+    }
+
+    if (!values.guests) {
+      errors.guests = "Number of guests is required";
+    } else if (values.guests > resortData.no_of_guest) {
+      errors.guests = "Number of guests exceeds the maximum limit";
+    }
+
+    if (!values.name) {
+      errors.name = "Name is required";
+    }
+
+    if (!values.phone) {
+      errors.phone = "Phone number is required";
+    } else if (values.phone.length !== 10) {
+      errors.phone = "Phone number should be 10 digits";
+    }
+
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: {
       dates: [],
@@ -160,50 +47,31 @@ function BookingData() {
       name: "",
       phone: "",
     },
-    validate: (values) => {
-      const errors = {};
-
-      if (!values.dates || values.dates.length === 0) {
-        errors.dates = "Please select a date range";
-      }
-
-      if (!values.guests) {
-        errors.guests = "Number of guests is required";
-      } else if (values.guests < 1) {
-        errors.guests = "Number of guests must be at least 1";
-      } else if (values.guests > resortData.no_of_guest) {
-        errors.guests = `Number of guests cannot exceed ${resortData.no_of_guest}`;
-      }
-
-      if (!values.name) {
-        errors.name = "Name is required";
-      }
-
-      if (!values.phone) {
-        errors.phone = "Phone number is required";
-      }else if (values.phone.length !== 10) {
-        errors.phone = "Phone number should be 10 digits";
-      }
-
-      return errors;
-    },
+    validate: validateForm,
     onSubmit: async (values) => {
-      try {
-        const { no_of_days } = values;
-        let bookingData = {
-          dates: values.dates,
-          guests: values.guests,
-          name: values.name,
-          phone: values.phone,
-          no_of_days: no_of_days,
-        };
-        dispatch(setBookingData(bookingData));
-        navigate("/bookingdetails");
-      } catch (error) {
-        console.log(error);
+      const errors = validateForm(values);
+      formik.setErrors(errors);
+
+      if (Object.keys(errors).length === 0) {
+        try {
+          const { no_of_days } = values;
+          const bookingData = {
+            dates: values.dates,
+            guests: values.guests,
+            name: values.name,
+            phone: values.phone,
+            no_of_days: no_of_days,
+          };
+
+          dispatch(setBookingData(bookingData));
+          navigate("/bookingdetails");
+        } catch (error) {
+          console.log(error);
+        }
       }
     },
   });
+
   const handleChange = (selectedDates) => {
     const numberOfDays = selectedDates[1].diff(selectedDates[0], "days") + 1;
     const startDate = moment(selectedDates[0].toDate()).format("DD-MM-YYYY");
@@ -221,7 +89,6 @@ function BookingData() {
     try {
       const response = await disabledDateList(resortData._id);
       if (response.success) {
-        console.log(response);
         setDisabledDates(response.disabledDates);
       } else {
         console.log(response.data.message);
@@ -257,7 +124,7 @@ function BookingData() {
           style={{ width: "100%" }}
           disabledDate={disabledDate}
         />
-        {formik.errors.dates && (
+        {formik.touched.dates && formik.errors.dates && (
           <div className="text-red-500">{formik.errors.dates}</div>
         )}
       </div>
@@ -265,13 +132,13 @@ function BookingData() {
         <input
           className="w-full px-4 py-3 placeholder-gray-500 border rounded-lg"
           type="number"
-          placeholder="No:of guests"
+          placeholder="No. of guests"
           name="guests"
           value={formik.values.guests}
           onChange={formik.handleChange}
           max={resortData.no_of_guest}
         />
-        {formik.errors.guests && (
+        {formik.touched.guests && formik.errors.guests && (
           <div className="text-red-500">{formik.errors.guests}</div>
         )}
       </div>
@@ -284,7 +151,7 @@ function BookingData() {
           value={formik.values.name}
           onChange={formik.handleChange}
         />
-        {formik.errors.name && (
+        {formik.touched.name && formik.errors.name && (
           <div className="text-red-500">{formik.errors.name}</div>
         )}
       </div>
@@ -297,7 +164,7 @@ function BookingData() {
           value={formik.values.phone}
           onChange={formik.handleChange}
         />
-        {formik.errors.phone && (
+        {formik.touched.phone && formik.errors.phone && (
           <div className="text-red-500">{formik.errors.phone}</div>
         )}
       </div>
@@ -315,3 +182,5 @@ function BookingData() {
 }
 
 export default BookingData;
+
+
