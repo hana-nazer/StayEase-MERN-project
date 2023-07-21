c868d5dcae36c536d33a01d91e65922d8120c8df
hana-nazer <hanapp9966@gmail.com>
2023-07-21 10:49:24 +0530

    Handle user chat error

--- client/src/pages/user/ChatList.js
+++ client/src/pages/user/ChatList.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Contacts from "../chats/Contacts";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { useSelector } from "react-redux";
 
 function ChatList() {
+  const currentUser = useSelector((state) => state.getUser.getUser);
+  const role = currentUser.role;
+  const name = currentUser.name;
   return (
     <>
-    <div className="fixed top-0 right-0 p-4">
-    <Contacts  role="user"/>
-    </div>
-     
+      {/* <div className="fixed top-0 right-0 p-4"> */}
+      <Navbar role={role} name={name} />
+      <Contacts role="user" />
+      <Footer />
+      {/* </div> */}
     </>
   );
 }
